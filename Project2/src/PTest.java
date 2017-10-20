89678d9188ba072eec28e29cd8db4b32b8ddd01f
Sippfi <s.seifert.97@web.de>
2017-10-20 15:38:08 +0200

    test_1

--- Project2/src/PTest.java
+++ Project2/src/PTest.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PTest {
+	Project example;
+	
+	 @Before
+	  public void setUp(){
+		 example = new Project(new String[][]{{"A", "B"}, {"B", "C"}});
+	 }
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_example_1() {
+		assertTrue(example.isWellSorted(new String[]{"A","B","C"}));
+	}
+	
+	@Test
+	public void test_example_2(){
+		assertFalse(example.isWellSorted(new String[]{"C","B","A"}));
 	}
 
 }
